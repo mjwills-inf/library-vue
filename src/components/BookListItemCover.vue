240ad4e03330e3a806ae72e02558f3bf6b16e7fc
mjwills-inf <mjwills@live.co.uk>
2020-12-04 17:08:37 +0000

    .

--- src/components/BookListItemCover.vue
+++ src/components/BookListItemCover.vue
@@ -8,8 +8,18 @@ export default {
   props: ["bookItem"],
   methods: {},
   created() {
-    console.log("created");
+    const getCover = async () => {
+      if (this.bookItem.cover === "") {
+        console.log("created, cover = '' ");
+        let data = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${this.bookItem.isbn}`)
+          .then(response=>response.json(), reason =>console.log(reason))
+        console.log(data)
+      }
+    }
+    
+   getCover()
     // add url from API to books array
+    // https://www.googleapis.com/books/v1/volumes?q=isbn:
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/-TOP-library-vue/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/-TOP-library-vue/" : "/"
+};
