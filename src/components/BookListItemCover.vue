ba399e787ed54b14a85d921dfa8b2654fece3859
mjwills-inf <mjwills@live.co.uk>
2020-12-06 13:46:45 +0000

    promise error

--- src/components/BookListItemCover.vue
+++ src/components/BookListItemCover.vue
@@ -6,18 +6,33 @@
 export default {
   name: "BookListItemCover",
   props: ["bookItem"],
-  methods: {},
-  created() {
-    const getCover = async () => {
-      if (this.bookItem.cover === "") {
-        console.log("created, cover = '' ");
-        let data = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${this.bookItem.isbn}`)
-          .then(response=>response.json(), reason =>console.log(reason))
-        console.log(data)
-      }
+  methods: {
+    async getCover()  {    
+      let responseData = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${this.bookItem.isbn}`)
+        .then(response=>response.json(), reason =>console.log(reason))
+      console.log(responseData)
+      if (responseData.totalItems === 0) {
+        console.log("No record")
+        return
+      } else {
+        this.data.foundCover = true;
+        this.data.url = `${responseData.items[0].volumeInfo.imageLinks.smallThumbnail}`
+        console.log("found")
+      }      
     }
-    
-   getCover()
+  },
+  data() {
+    return {
+      isbnPresent: false,
+      foundCover: false,
+      url: ""
+    }
+  },
+  created() {    
+      if (this.bookItem.cover === "") {
+        console.log("call getCover")
+        this.getCover();
+      }    
     // add url from API to books array
     // https://www.googleapis.com/books/v1/volumes?q=isbn:
   }
